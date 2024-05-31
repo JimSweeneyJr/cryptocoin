dc723e00f37954797bea1f0ddb096bd11bcf0993
Jim Sweeney <131217444+JimSweeneyJr@users.noreply.github.com>
2024-05-30 22:11:39 -0400

    parsing incoming data, threading and error handling

--- cryptocoin/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ cryptocoin/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
 class CoinsViewModel: ObservableObject {
     @Published var coin = ""
     @Published var price = ""
+    @Published var errorMessage: String?
     
     init() {
         fetchPrice(coin: "bitcoin")
@@ -19,21 +20,34 @@ class CoinsViewModel: ObservableObject {
         let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
         guard let url = URL(string: urlString) else { return }
         
-        print("Fetching price....")
         URLSession.shared.dataTask(with: url) { data, response, error in
-            print("Did receive data \(String(describing: data))")
-            guard let data = data else { return }
-            guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
-            guard let value = jsonObject[coin] as? [String: Int] else { return }
-            guard let price = value["usd"] else { return }
-            
-            DispatchQueue.main.async {
+            DispatchQueue.main.sync {
+                if let error = error {
+                    print("DEBUG: Failed with errro \(error.localizedDescription)")
+                    self.errorMessage = error.localizedDescription
+                    return
+                }
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    self.errorMessage = "Bad HTTP Response."
+                    return
+                }
+                
+                guard httpResponse.statusCode == 200 else {
+                    self.errorMessage = "Failed to fetch with status code \(httpResponse.statusCode)"
+                    return
+                }
+                
+                guard let data = data else { return }
+                guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
+                guard let value = jsonObject[coin] as? [String: Double] else {
+                    print("Failed to parse value")
+                    return
+                }
+                guard let price = value["usd"] else { return }
+                
                 self.coin = coin.capitalized
                 self.price = "$\(price)"
             }
         }.resume()
-        
-        print("Did reach end of function")
     }
-    
 }
